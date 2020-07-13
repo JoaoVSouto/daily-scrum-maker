c0955605d09f59ce76706f2e7f847aeef562575c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-12 23:37:44 -0300

    Handles form data and copies it to clipboard

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,13 +1,47 @@
 import React, { useState } from 'react';
+import { SubmitHandler } from '@unform/core';
+import copy from 'copy-to-clipboard';
 
 import Textarea from '../Textarea';
 
 import { Container, Form as Unform, Button } from './styles';
 
+interface FormData {
+  didLastExpedient: string;
+  difficulties: string;
+  doingToday: string;
+  howAmI: string;
+}
+
 const Form: React.FC = () => {
   const [activated, setActivated] = useState(false);
 
-  const handleSubmit = () => {};
+  const handleSubmit: SubmitHandler<FormData> = data => {
+    const { didLastExpedient, difficulties, doingToday, howAmI } = data;
+
+    const titlesAndContents = [
+      ['O que fiz no último expediente?', didLastExpedient],
+      ['Dificuldades?', difficulties],
+      ['O que pretendo fazer hoje?', doingToday],
+      ['Como estou?', howAmI],
+    ];
+
+    const titlesAndContentsStringified = titlesAndContents
+      .reduce(
+        (accumulatedText, [title, content]) => `${accumulatedText}
+          *${title}*
+          ${content}
+          `,
+        ''
+      )
+      .split('\n')
+      .map(line => line.trim())
+      .join('\n');
+
+    const dailyScrum = `*Daily Scrum*\n${titlesAndContentsStringified}`;
+
+    copy(dailyScrum);
+  };
 
   return (
     <Container>
@@ -20,7 +54,12 @@ const Form: React.FC = () => {
         <Textarea name="doingToday" label="O que pretendo fazer hoje?" />
         <Textarea name="howAmI" label="Como estou fisicamente e mentalmente?" />
 
-        <Button activated={activated} onClick={() => setActivated(!activated)}>
+        <Button
+          type="submit"
+          activated={activated}
+          onClick={() => setActivated(true)}
+          onBlur={() => setActivated(false)}
+        >
           <span>Copiar</span>
           <div className="wave" />
         </Button>
