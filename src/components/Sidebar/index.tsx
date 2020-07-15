fdbfdec520c7ef423ec39465544388ae7b9df137
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-14 22:57:17 -0300

    Creates toggle theme button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  ToggleThemeContainer,
+  ToggleThemeCheckbox,
+  ToggleThemeLabel,
+} from './styles';
 
 const Sidebar: React.FC = () => {
   return (
     <Container>
       <h1>Daily Scrum Maker</h1>
+
+      <ToggleThemeContainer>
+        <ToggleThemeCheckbox id="toggle-theme" defaultChecked />
+        <ToggleThemeLabel htmlFor="toggle-theme">
+          <span />
+        </ToggleThemeLabel>
+      </ToggleThemeContainer>
     </Container>
   );
 };
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -21,3 +21,183 @@ export const Container = styled.div`
     margin-bottom: 16px;
   }
 `;
+
+export const ToggleThemeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 24px;
+
+  @media (max-width: 768px) {
+    margin-top: 16px;
+  }
+`;
+
+export const ToggleThemeCheckbox = styled.input.attrs({
+  type: 'checkbox',
+})`
+  height: 0;
+  transition: all 250ms ease-in;
+
+  &:focus + label {
+    box-shadow: 0 0 0 4px #fff;
+  }
+
+  &:checked + label {
+    background-color: #9ee3fb;
+    border: 5px solid #86c3d7;
+
+    &::before {
+      left: 55px;
+      background-color: #ffdf6d;
+      border: 5px solid #e1c348;
+    }
+
+    &::after {
+      opacity: 100;
+      animation-name: bounce_in;
+      animation-duration: 0.6s;
+      animation-delay: 0.1s;
+      animation-fill-mode: backwards;
+      animation-timing-function: ease-in-out;
+    }
+
+    > span {
+      opacity: 0;
+      box-shadow: rgba(255, 255, 255, 0.1) 30px -3px 0 -4px,
+        rgba(255, 255, 255, 0.1) 12px 10px 0 -5px, #fff 38px 18px 0 -3px,
+        rgba(255, 255, 255, 0.1) 32px 34px 0 -4px, #fff 20px 24px 0 -5.5px,
+        rgba(255, 255, 255, 0.1) 5px 38px 0 -3px;
+      animation: none;
+
+      &::before {
+        left: 25px;
+        transform: rotate(70deg);
+      }
+    }
+  }
+
+  @keyframes bounce_in {
+    0% {
+      opacity: 0;
+      transform: scale(0.3);
+    }
+
+    50% {
+      opacity: 100;
+      transform: scale(1.1);
+    }
+
+    55% {
+      transform: scale(1.1);
+    }
+
+    75% {
+      transform: scale(0.9);
+    }
+
+    100% {
+      opacity: 100;
+      transform: scale(1);
+    }
+  }
+`;
+
+// https://codepen.io/ashleynolan/pen/wBppKz
+export const ToggleThemeLabel = styled.label`
+  display: block;
+  transition: all 250ms ease-in;
+  position: relative;
+  border-radius: 70px;
+  width: 125px;
+  height: 70px;
+  background-color: #3c4145;
+  border: 5px solid #1c1c1c;
+  cursor: pointer;
+
+  &::before {
+    box-sizing: border-box;
+    content: '';
+    display: block;
+    transition: all 250ms ease-in;
+    border-radius: 50%;
+    width: 56px;
+    height: 56px;
+    position: absolute;
+    top: 2px;
+    left: 4px;
+    border: 5px solid #e3e3c7;
+    background-color: #fff;
+  }
+
+  &::after {
+    box-sizing: border-box;
+    content: '';
+    display: block;
+    transition: opacity 100ms ease-in;
+    position: absolute;
+    top: 62%;
+    left: 39px;
+    z-index: 10;
+    width: 11.2px;
+    height: 11.2px;
+    opacity: 0;
+    background-color: #fff;
+    border-radius: 50%;
+    box-shadow: #fff 0 0, #fff 3px 0, #fff 6px 0, #fff 9px 0, #fff 11px 0,
+      #fff 14px 0, #fff 16px 0, #fff 21px -1px 0 1px, #fff 16px -7px 0 -2px,
+      #fff 7px -7px 0 1px, #d3d3d3 0 0 0 4px, #d3d3d3 6px 0 0 4px,
+      #d3d3d3 11px 0 0 4px, #d3d3d3 16px 0 0 4px, #d3d3d3 21px -1px 0 5px,
+      #d3d3d3 16px -7px 0 1px, #d3d3d3 7px -7px 0 5px;
+  }
+
+  > span {
+    display: block;
+    position: absolute;
+    top: 9px;
+    left: 52.5%;
+    transition: all 250ms ease-in;
+    background-color: #fff;
+    border-radius: 50%;
+    width: 4px;
+    height: 4px;
+    z-index: 20;
+    box-shadow: rgba(255, 255, 255, 0.1) 30px -3px 0 0,
+      rgba(255, 255, 255, 0.1) 12px 10px 0 -1px, #fff 38px 18px 0 1px,
+      rgba(255, 255, 255, 0.1) 32px 34px 0 0, #fff 20px 24px 0 -1.5px,
+      rgba(255, 255, 255, 0.1) 5px 38px 0 1px;
+    animation: starry_star 5s ease-in-out infinite;
+
+    &::before {
+      box-sizing: border-box;
+      content: '';
+      display: block;
+      transition: all 250ms ease-in;
+      position: absolute;
+      top: -2px;
+      left: -25px;
+      width: 18px;
+      height: 18px;
+      background-color: #fff;
+      border-radius: 50%;
+      border: 5px solid #e3e3c7;
+      box-shadow: #e3e3c7 -28px 0 0 -3px, #e3e3c7 -8px 24px 0 -2px;
+      transform-origin: -6px 130%;
+    }
+
+    &::after {
+      box-sizing: border-box;
+      content: '';
+      display: block;
+      transition: all 250ms ease-in;
+    }
+  }
+
+  @keyframes starry_star {
+    50% {
+      background-color: rgba(255, 255, 255, 0.1);
+      box-shadow: #fff 30px -3px 0 0, #fff 12px 10px 0 -1px,
+        rgba(255, 255, 255, 0.1) 38px 18px 0 1px, #fff 32px 34px 0 0,
+        rgba(255, 255, 255, 0.1) 20px 24px 0 -1.5px, #fff 5px 38px 0 1px;
+    }
+  }
+`;
