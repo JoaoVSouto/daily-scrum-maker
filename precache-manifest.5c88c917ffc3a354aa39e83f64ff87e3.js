9538bc5ea3229a6df8baf808fab70bc37a9bb067
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-15 19:58:11 -0300

    Updates

---
+++ precache-manifest.5c88c917ffc3a354aa39e83f64ff87e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef9c5962d293e532fa92d0a5191a49e",
+    "url": "/daily-scrum-maker/index.html"
+  },
+  {
+    "revision": "42bad6748aae6e95a293",
+    "url": "/daily-scrum-maker/static/css/2.51270f71.chunk.css"
+  },
+  {
+    "revision": "42bad6748aae6e95a293",
+    "url": "/daily-scrum-maker/static/js/2.57536d03.chunk.js"
+  },
+  {
+    "revision": "e3b2ee499c95ae4c1222aeba4b937bcb",
+    "url": "/daily-scrum-maker/static/js/2.57536d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfba3252770ddf425d5e",
+    "url": "/daily-scrum-maker/static/js/main.0873c4f7.chunk.js"
+  },
+  {
+    "revision": "b0936dd1009e236e9012",
+    "url": "/daily-scrum-maker/static/js/runtime-main.155c100c.js"
+  },
+  {
+    "revision": "2ad1623949baa5ba35de2cb341913474",
+    "url": "/daily-scrum-maker/static/media/cloud.2ad16239.svg"
+  },
+  {
+    "revision": "bbefb38a85f99dc33d3b14e914d5aad9",
+    "url": "/daily-scrum-maker/static/media/cloud_dark.bbefb38a.svg"
+  },
+  {
+    "revision": "31d65b3b0dfc33e1c8e424c1d356236e",
+    "url": "/daily-scrum-maker/static/media/many_clouds.31d65b3b.svg"
+  },
+  {
+    "revision": "ee0284bae094dcd4af1b1fe48c9cf59f",
+    "url": "/daily-scrum-maker/static/media/many_clouds_dark.ee0284ba.svg"
+  },
+  {
+    "revision": "d963b8944752859b3c6ccb0bda539c46",
+    "url": "/daily-scrum-maker/static/media/stars.d963b894.svg"
+  }
+]);
