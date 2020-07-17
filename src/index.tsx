26d9cd77167af3b42173c62165a9c5db37e449bb
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-17 15:21:42 -0300

    Adds analytics tracking data

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import App from './pages/Home';
 
 import 'simplebar/dist/simplebar.min.css';
+
+ReactGA.initialize('UA-172963192-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
