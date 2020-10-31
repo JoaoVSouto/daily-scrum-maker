e77e0993c4e14203ad56a5c00ee6f4bf913ef991
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-31 13:37:05 -0300

    Changes add topic and subtopic icons

--- src/components/FormWithTopics/index.tsx
+++ src/components/FormWithTopics/index.tsx
@@ -1,9 +1,8 @@
 import React, { Fragment, useState, useRef, useCallback } from 'react';
 import { SubmitHandler, FormHandles } from '@unform/core';
 import copy from 'copy-to-clipboard';
-import * as Yup from 'yup';
 import { nanoid } from 'nanoid';
-import { FiPlus } from 'react-icons/fi';
+import { FiEdit2, FiEdit3 } from 'react-icons/fi';
 
 import Textarea from '../Textarea';
 
@@ -19,22 +18,13 @@ interface FormData {
 
 interface Subtopic {
   id: string;
-  value: string;
 }
 
 interface Topic {
   id: string;
-  title: string;
   label?: string;
   subtopics: Subtopic[];
 }
-
-const schema = Yup.object().shape({
-  didLastExpedient: Yup.string().required('Esse campo é obrigatório'),
-  difficulties: Yup.string().required('Esse campo é obrigatório'),
-  doingToday: Yup.string().required('Esse campo é obrigatório'),
-  howAmI: Yup.string().required('Esse campo é obrigatório'),
-});
 
 const FormWithTopics: React.FC = () => {
   const unformRef = useRef<FormHandles>(null);
@@ -44,7 +34,6 @@ const FormWithTopics: React.FC = () => {
     [
       {
         id: nanoid(),
-        title: '',
         label: 'O que fiz no último expediente?',
         subtopics: [],
       },
@@ -52,69 +41,36 @@ const FormWithTopics: React.FC = () => {
   );
 
   const handleSubmit: SubmitHandler<FormData> = async data => {
-    try {
-      const { didLastExpedient, difficulties, doingToday, howAmI } = data;
+    const { didLastExpedient, difficulties, doingToday, howAmI } = data;
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+    setActivated(true);
 
-      setActivated(true);
+    const titlesAndContents = [
+      ['O que fiz no último expediente?', didLastExpedient],
+      ['Dificuldades?', difficulties],
+      ['O que pretendo fazer hoje?', doingToday],
+      ['Como estou fisicamente e mentalmente?', howAmI],
+    ];
 
-      const titlesAndContents = [
-        ['O que fiz no último expediente?', didLastExpedient],
-        ['Dificuldades?', difficulties],
-        ['O que pretendo fazer hoje?', doingToday],
-        ['Como estou fisicamente e mentalmente?', howAmI],
-      ];
-
-      const titlesAndContentsStringified = titlesAndContents
-        .reduce(
-          (accumulatedText, [title, content]) => `${accumulatedText}
+    const titlesAndContentsStringified = titlesAndContents
+      .reduce(
+        (accumulatedText, [title, content]) => `${accumulatedText}
             *${title}*
             ${content}
             `,
-          ''
-        )
-        .split('\n')
-        .map(line => line.trim())
-        .join('\n');
+        ''
+      )
+      .split('\n')
+      .map(line => line.trim())
+      .join('\n');
 
-      const dailyScrum = `*Daily Scrum*\n${titlesAndContentsStringified}`;
-      copy(dailyScrum);
-    } catch (err) {
-      const validationErrors: {
-        [error: string]: string;
-      } = {};
-
-      if (err instanceof Yup.ValidationError) {
-        err.inner.forEach(error => {
-          validationErrors[error.path] = error.message;
-        });
-
-        unformRef.current?.setErrors(validationErrors);
-      }
-    }
-  };
-
-  const handleInputChange = (inputName: keyof FormData): void => {
-    const inputValue = unformRef.current?.getFieldValue(inputName) as
-      | string
-      | undefined;
-
-    if (inputValue?.trim()) {
-      const currentErrors = unformRef.current?.getErrors() || {};
-
-      delete currentErrors[inputName];
-
-      unformRef.current?.setErrors(currentErrors);
-    }
+    const dailyScrum = `*Daily Scrum*\n${titlesAndContentsStringified}`;
+    copy(dailyScrum);
   };
 
   const handleAddNewDidLastExpedientTopic = useCallback(() => {
     const newTopic: Topic = {
       id: nanoid(),
-      title: '',
       subtopics: [],
     };
 
@@ -125,7 +81,6 @@ const FormWithTopics: React.FC = () => {
     (id: string) => {
       const newSubtopic: Subtopic = {
         id: nanoid(),
-        value: '',
       };
 
       const updatedDidLastExpedientTopics = [...didLastExpedientTopics];
@@ -170,7 +125,7 @@ const FormWithTopics: React.FC = () => {
               }
             >
               Novo subtópico
-              <FiPlus size={24} />
+              <FiEdit3 size={16} />
             </AddSubtopicButton>
           </Fragment>
         ))}
@@ -179,26 +134,14 @@ const FormWithTopics: React.FC = () => {
           onClick={handleAddNewDidLastExpedientTopic}
         >
           Novo tópico
-          <FiPlus size={24} />
+          <FiEdit2 size={16} />
         </AddTopicButton>
 
-        <Textarea
-          name="difficulties"
-          label="Dificuldades?"
-          changeHandler={handleInputChange}
-        />
+        <Textarea name="difficulties" label="Dificuldades?" />
 
-        <Textarea
-          name="doingToday"
-          label="O que pretendo fazer hoje?"
-          changeHandler={handleInputChange}
-        />
+        <Textarea name="doingToday" label="O que pretendo fazer hoje?" />
 
-        <Textarea
-          name="howAmI"
-          label="Como estou fisicamente e mentalmente?"
-          changeHandler={handleInputChange}
-        />
+        <Textarea name="howAmI" label="Como estou fisicamente e mentalmente?" />
 
         <Button
           type="submit"
--- src/components/FormWithTopics/styles.ts
+++ src/components/FormWithTopics/styles.ts
@@ -8,7 +8,6 @@ const BaseTopicButton = styled.button`
   color: #fff;
   border: 2px solid transparent;
   padding: 6px;
-  padding-left: 10px;
   margin-top: 8px;
   border-radius: 4px;
   transition: all 0.3s;
@@ -23,12 +22,12 @@ const BaseTopicButton = styled.button`
     border-color: #fff;
 
     svg {
-      transform: rotate(90deg);
+      transform: rotate(45deg);
     }
   }
 
   svg {
-    margin-left: 4px;
+    margin-left: 6px;
     transition: transform 0.3s;
   }
 `;
